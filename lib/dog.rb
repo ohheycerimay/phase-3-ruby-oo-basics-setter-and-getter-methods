91e1876f77ce3670aa70e6acdeaa734ec1304cf9
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-11-22 12:26:58 -0500

    commit tree 99ef898794c0d02c3da350bd66a7cece7f0112e2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+    snoopy = Dog.new
+    snoopy.breed = "Beagle"
 end
